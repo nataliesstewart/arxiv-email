ba7c29fb9b600034f8148bbd2fbadc41626cf639
tbrazel <42276623+tbrazel@users.noreply.github.com>
2024-02-06 13:42:41 -0500

    updated to be compatible with changes to arxiv API
    https://blog.arxiv.org/2024/01/31/attention-arxiv-users-re-implemented-rss/

--- daily-preprint.py
+++ daily-preprint.py
@@ -13,8 +13,8 @@ today = date.today()
 
 # Get a list of math subjects and math tags from 'subj-list.csv'
 with open('subj-list.csv','r') as f:
-    reader = csv.reader(f)
-    mathSubjects = list(reader)
+	reader = csv.reader(f)
+	mathSubjects = list(reader)
 
 all_possible_tags = [x[0] for x in mathSubjects]
 all_possible_subjects = [x[1] for x in mathSubjects]
@@ -36,14 +36,14 @@ html_top = """\
 		#body {
 			font-size: 14px;
 		}
-	  	#banner{
-	  		width:100%;
-	  		font-size:18px;
-	  		color: #F4F2F3;
-	  		text-align:center;
-	  		padding:1px;
-	  		border-radius: 20px;
-	  		background-color: #515c5d;
+		#banner{
+			width:100%;
+			font-size:18px;
+			color: #F4F2F3;
+			text-align:center;
+			padding:1px;
+			border-radius: 20px;
+			background-color: #515c5d;
 		}
 
 		.banner_table{
@@ -104,23 +104,23 @@ html_top = """\
 		   z-index: 2;
 		   position: absolute;
 		   top: 50%;
-		    left: 50%;
-		    right: 50%;
-		    font-size: 20px;
-		    width: inherit;
-		    padding: 0;
-		    margin: 0;
+			left: 50%;
+			right: 50%;
+			font-size: 20px;
+			width: inherit;
+			padding: 0;
+			margin: 0;
 		}
 
 		.container img{
-		    z-index: 1;
-		    width: 100%;
-		    height: 100%;
-		    position: absolute;
-		    top:0;
-		    right:0;
-		    bottom:0;
-		    left:0;
+			z-index: 1;
+			width: 100%;
+			height: 100%;
+			position: absolute;
+			top:0;
+			right:0;
+			bottom:0;
+			left:0;
 		}
 
 
@@ -161,40 +161,51 @@ for subj in subscriptions:
 
 	for entry in pointer:
 
-		##
+		# Get the title of the paper
 		papertitle = str(entry.title)
-		papersplit = papertitle.split('arXiv')
-		papersplit[0] = papersplit[0][:-3]
-		papersplit[1] = papersplit[1][11:]
 
-		list_of_tags = []
+		# Get the primary tag (note that entry.tags is a list)
+		# The API convention, from what I can understand, lists the primary posting as the first tag
+		primarytag = entry.tags[0].term
 
 
-		# Figure out what the associated tag (e.g. "math.AG") is for the given paper
-		for x in mathSubjects:
-			if x[0] in papersplit[1]:
-				list_of_tags.append(x[1])
-
-		# Make HTML for each tag for the paper
+		# Make the list of tags, and the associated html
 		tags_html = ''
-		for tag in list_of_tags:
-			# Check if the paper was crosslisted by seeing if the tag is the same as the current subject the API is querying
-			if tag == subj_title:
+		for x in entry.tags:
+			tag = x.term
+			if tag == primarytag:
 				newtag = '<div id="tag">' + str(tag) + '</div>'
 			else:
 				newtag = '<div id="xlistedtag">' + str(tag) + '</div>'
 			
 			tags_html = tags_html + newtag
-		
 
-		# Make HTML to see the paper version
-		version = papersplit[1].split(' ')[0]
+
+		# Strip the entry.id at 'v' to get the version number
+		# new entries by convention have v1
+		version_number = entry.id.split('v')[2]
+
 		version_html = ''
-		if version != 'v1':
-			version_html = version_html + '<div id="updated">' + str(version) + ' UPDATED</div>'
+		if version_number != 1:
+			version_html + '<div id="updated">v' + str(version_number) + '</div>'
 
-		##
-		entry_html = '\n\t\t<div id="paper"><b>Title:</b> <a id="paperTitle" href="' + str(entry.id) + '">' + str(papersplit[0]) + '</a>' + tags_html + version_html + '<br>\n\t<b> Authors: </b>' + str(entry.author) + '<br>\n\t\t' + str(entry.summary) + '</div>\n<hr>'
+		# Make the html for the entry
+		entry_html = '\n\t\t<div id="paper"><b>Title:</b> '
+
+		# Add the link to the title
+		entry_html = entry_html + '<a id="paperTitle" href="' + str(entry.link) + '">' + papertitle + '</a>'
+		
+		# Add tags and version info
+		entry_html = entry_html + tags_html + version_html
+
+		# Add authors
+		entry_html = entry_html + '<br>\n\t<b> Authors: </b>' + str(entry.author) 
+
+		# Add summary
+		entry_html = entry_html	+ '<br>\n\t\t' + str(entry.summary) + '</div>\n<hr>'
+
+
+		# Add alternative text version of email
 		entry_text = str(entry.title) + '\n\t' + str(entry.author) + '\n\n\t\t' + str(entry.summary)
 
 		rss_html = rss_html + entry_html
