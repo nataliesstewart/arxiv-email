c39b02799275795e66c9283ce6e95c46f27073d9
tbrazel <42276623+tbrazel@users.noreply.github.com>
2024-02-06 15:37:06 -0500

    testing email trigger with personal info

--- freeloaders.py
+++ freeloaders.py
@@ -7,12 +7,11 @@ load_dotenv(".env")
 
 USER = os.environ.get("GMAIL_USER")
 PASSWORD = os.environ.get("GMAIL_PASSWORD")
-
 FRELOADERS = os.environ.get("FREELOADERS")
 
 
-# tbraz56@gmail.com,math.AT,math.AG
-# tbraz56+1@gmail.com,math.NT
+# tbraz56+1@gmail.com:math.AT,math.AG
+# tbraz56+2@gmail.com:math.NT
 
 
 print('freeloaders var is ' + str(FRELOADERS))
@@ -20,5 +19,9 @@ print('freeloaders var is ' + str(FRELOADERS))
 list_of_recipients = FRELOADERS.split('\n')
 print(' list of recipients is ' + str(list_of_recipients))
 
+for x in list_of_recipients:
+	recipient = x.split(':')[0]
+	subjs = x.split(':')[1]
 
-
+	subjprefs = subjs.split(',')
+	send_email(recipient,subjprefs,USER,PASSWORD)
