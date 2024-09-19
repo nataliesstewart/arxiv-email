6c055f49867a36050b3aa5e1b695138c198f4b1b
Natalie Stewart <47867595+nataliesstewart@users.noreply.github.com>
2024-09-19 10:54:03 -0400

    followint tommy's instructions

--- daily-preprint.py
+++ daily-preprint.py
@@ -8,7 +8,7 @@ load_dotenv(".env")
 USER = os.environ.get("GMAIL_USER")
 PASSWORD = os.environ.get("GMAIL_PASSWORD")
 RECIPIENT = os.environ.get("RECIPIENT")
-subscriptions = ["math.AT","math.AG"]
+subscriptions = ["math.AT","math.CT","math.KT"]
 
 
 send_email(RECIPIENT,subscriptions,USER,PASSWORD)
